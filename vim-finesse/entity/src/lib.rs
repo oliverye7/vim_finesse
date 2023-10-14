c3d5419111547caa39287ae7bae5a55cf546312a
Sean Yang <seanyang12345@gmail.com>
2023-10-14 16:13:17 -0700

    init

---
+++ vim-finesse/entity/src/lib.rs
@@ -0,0 +1,17 @@
+pub use async_graphql;
+pub use sea_orm;
+
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
