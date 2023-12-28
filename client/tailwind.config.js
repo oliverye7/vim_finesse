d2fb175185227e467b6a8a1f850636f8cc4ac144
OLIVER YE <oliverye@berkeley.edu>
2023-12-27 17:44:58 -0800

    create site color palette

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,6 +5,18 @@ module.exports = {
       fontFamily: {
         Poppins: "'Poppins', sans",
       },
+      colors: {
+        "site-red": "#fb7da7",
+        "site-purple": "#a18bd3",
+        "site-yellow": "#ffce5b",
+        "site-cyan": "#50c6d8",
+        "site-green": "#2cda9d",
+        "site-darkgreen": "#4c8273",
+        "site-menublack": "#1c1c1c",
+        "site-grey": "#818e8e",
+        "site-white": "#efeeea",
+        "site-black": "#2f343f",
+      },
     },
   },
   plugins: [],
